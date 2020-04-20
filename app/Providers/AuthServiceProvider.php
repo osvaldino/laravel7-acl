ecb4cc23ccc217de4c1fbde42859fb6bd57e568a
Osvaldino Neto <alfnet.design@gmail.com>
2020-04-20 14:54:31 -0400

    fixed Gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider {
 	/**
@@ -22,7 +23,6 @@ class AuthServiceProvider extends ServiceProvider {
 	public function boot() {
 		$this->registerPolicies();
 
-		// Implicitly grant "Super Admin" role all permission checks using can()
 		Gate::before(function ($user, $ability) {
 			if ($user->hasRole('Super-Admin')) {
 				return true;
